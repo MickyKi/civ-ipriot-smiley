9d74aea010eb1281a058b1ecb0119662cfd9e8d6
MickyKi <V127658@tafe.wa.edu.au>
2025-12-13 13:07:37 +0800

    angry class updates added

--- angry.py
+++ angry.py
@@ -1,1 +1,24 @@
+import time
+from smiley import Smiley
+
+class Angry(Smiley):
+    def __init__(self):
+        super().__init__(complexion=self.RED)   # Angry face is red
+        self.draw_angry_face()
+
+    def draw_mouth(self):
+        mouth = [41, 42, 43, 44, 45, 46]  # straight mouth
+        for pixel in mouth:
+            self.pixels[pixel] = self.BLANK
+
+    def draw_eyes(self):
+        eye_pixels = [10, 13, 18, 21]
+        for pixel in eye_pixels:
+            self.pixels[pixel] = self.RED
+
+    def draw_angry_face(self):
+        self.pixels = [self.my_complexion] * 64
+        self.draw_mouth()
+        self.draw_eyes()
+        self.show()
 
