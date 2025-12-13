85dd9a2fd5c2ec150e68e501d2a4ba5504b13d60
MickyKi <V127658@tafe.wa.edu.au>
2025-12-13 12:39:29 +0800

    Refactor comments for clarity in main.py

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
-"""Demonstrates the use of the Smiley class and its subclasses.
+"""
+Demonstrates the use of the Smiley class and its subclasses.
 If you have access to a SenseHAT (either via a Raspberry Pi or a SenseHAT emulator),
 you can use the real SenseHAT class instead of the mock SenseHAT class.
 That is, delete the sense_hat.py file that is included in this bundle.
 """
 
 import time
-
 from happy import Happy
 from sad import Sad   
 
 def main():
-    # Tests the Happy Smiley
+    # Show Happy Smiley
     happy_face = Happy()
-    happy_face.draw_happy_face()   
     time.sleep(1)
     happy_face.blink()
 
-    # Tests the Sad Smiley
+    # Show Sad Smiley
     sad_face = Sad()
-    sad_face.draw_sad_face()       
     time.sleep(1)
     sad_face.blink()
-
 
 if __name__ == '__main__':
     ############################################################
