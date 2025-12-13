d226a3ea9938b1cd1edb6b4252eade06f717e89e
MickyKi <V127658@tafe.wa.edu.au>
2025-12-13 13:03:23 +0800

    Update smiley.py

--- smiley.py
+++ smiley.py
@@ -1,39 +1,29 @@
 from sense_hat import SenseHat
 
-
 class Smiley:
-    WHITE = (255, 255, 255)
-    GREEN = (0, 255, 0)
-    RED = (255, 0, 0)
+    # Class variables (shared constants)
+    BLACK  = (0, 0, 0)
+    BLANK  = BLACK
     YELLOW = (255, 255, 0)
-    BLANK = (0, 0, 0)
+    WHITE  = (255, 255, 255)
+    RED    = (255, 0, 0)
+    GREEN  = (0, 255, 0)
+    BLUE   = (0, 0, 255)
 
-    def __init__(self):
-        # We have encapsulated the SenseHat object
+    def __init__(self, complexion=YELLOW):
+        # Instance variables
         self.sense_hat = SenseHat()
-
-        Y = self.YELLOW
-        O = self.BLANK
-        self.pixels = [
-            O, Y, Y, Y, Y, Y, Y, O,
-            Y, Y, Y, Y, Y, Y, Y, Y,
-            Y, Y, Y, Y, Y, Y, Y, Y,
-            Y, Y, Y, Y, Y, Y, Y, Y,
-            Y, Y, Y, Y, Y, Y, Y, Y,
-            Y, Y, Y, Y, Y, Y, Y, Y,
-            Y, Y, Y, Y, Y, Y, Y, Y,
-            O, Y, Y, Y, Y, Y, Y, O,
-        ]
-
-    def dim_display(self, dimmed=True):
-        """
-        Set the SenseHat's light intensity to low (True) or high (False)
-        :param dimmed: Dim the display if True, otherwise don't dim
-        """
-        self.sense_hat.low_light = dimmed
+        self.my_complexion = complexion
+        self.pixels = [self.my_complexion] * 64
+        self.show()
 
     def show(self):
-        """
-        Show the smiley on the screen.
-        """
+        # Push current pixel buffer to display
         self.sense_hat.set_pixels(self.pixels)
+
+    def clear(self):
+        self.sense_hat.clear()
+
+    def complexion(self):
+        # Return the complexion colour for this smiley
+        return self.my_complexion
