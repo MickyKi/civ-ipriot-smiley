60df0facb449d4bfbda859bf6372c36dd209e65b
MickyKi <V127658@tafe.wa.edu.au>
2025-12-13 12:12:03 +0800

    Update happy.py

--- happy.py
+++ happy.py
@@ -20,24 +20,24 @@ class Happy(Smiley, Blinkable):
         mouth = [41, 46, 50, 51, 52, 53]
         for pixel in mouth:
             self.pixels[pixel] = self.BLANK
-
+            
     def draw_eyes(self, wide_open=True):
         """
-       Draws the eyes (open or closed) on the standard smiley.
-        :param wide_open (bool): eyes open or closed.
+        Draws the eyes (open or closed) on the happy smiley.
+        :param wide_open: eyes open or closed
         """
-        eyes = [10, 13, 18, 21]
-        for pixel in eyes:
-            self.pixels[pixel] = self.BLANK if wide_open else self.YELLOW
+        eye_pixels = [10, 13, 18, 21]
+        for pixel in eye_pixels:
+            colour = self.BLANK if wide_open else self.YELLOW
+            self.pixels[pixel] = colour
 
     def blink(self, delay=0.25):
         """
-       Blinks the smiley's eyes once
-        
-        :param delay: Delay between blinks (in seconds)
+        Blinks the happy smiley's eyes once
         """
         self.draw_eyes(wide_open=False)
         self.show()
         time.sleep(delay)
         self.draw_eyes(wide_open=True)
         self.show()
+
