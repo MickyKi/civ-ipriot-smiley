b09ea7dcaf4f59e6d360cceeff6a718f1ffe4d81
MickyKi <V127658@tafe.wa.edu.au>
2025-12-13 13:40:04 +0800

    updates

--- blinkable.py
+++ blinkable.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
 
-
 class Blinkable(ABC):
     """
     Specify what anything that claims to be 'blinkable' should be able to do.
     """
     @abstractmethod
-    def blink(self):
+    def blink(self, delay=0.25):
+        """
+        Abstract blink method — subclasses must implement their own blink behaviour.
+        """
         pass
