26928d557bab3e79880e96a2f656c820d8f8f5b6
MickyKi <V127658@tafe.wa.edu.au>
2025-12-13 13:04:33 +0800

    Integrate Blinkable functionality into Happy class

--- happy.py
+++ happy.py
@@ -1,46 +1,29 @@
 import time
 from smiley import Smiley
+from blinkable import Blinkable
 
-class Happy(Smiley):
-    """
-    Provides a Smiley with a happy expression
-    """
+class Happy(Smiley, Blinkable):
     def __init__(self):
-        super().__init__()
+        super().__init__(complexion=self.YELLOW)
         self.draw_happy_face()   # show the happy face immediately
 
     def draw_mouth(self):
-        """
-        Renders a smiling mouth by blanking the pixels that form that object.
-        """
         mouth = [41, 46, 50, 51, 52, 53]
         for pixel in mouth:
             self.pixels[pixel] = self.BLANK
 
     def draw_eyes(self, wide_open=True):
-        """
-        Draws the eyes (open or closed) on the happy smiley.
-        """
         eye_pixels = [10, 13, 18, 21]
         for pixel in eye_pixels:
             colour = self.BLANK if wide_open else self.YELLOW
             self.pixels[pixel] = colour
 
     def draw_happy_face(self):
-        """
-        Combines mouth and eyes into a full happy face
-        """
-        self.pixels = [self.YELLOW] * 64   # reset background
+        self.pixels = [self.my_complexion] * 64
         self.draw_mouth()
         self.draw_eyes()
         self.show()
 
     def blink(self, delay=0.25):
-        """
-        Blinks the happy smiley's eyes once
-        """
-        self.draw_eyes(wide_open=False)
-        self.show()
-        time.sleep(delay)
-        self.draw_eyes(wide_open=True)
-        self.show()
+        # Uses Blinkable dimming behaviour
+        super().blink(delay)
